1a725cb6da6f058aa24386dafc18a8aad9b263d1
Dan Shawver <drshawv@gmail.com>
2021-05-26 19:46:39 -0400

    Initial commit of maestro-app

---
+++ src/main/java/com/mmp/maestroapp/MaestroAppApplication.java
@@ -0,0 +1,13 @@
+package com.mmp.maestroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaestroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaestroAppApplication.class, args);
+	}
+
+}
